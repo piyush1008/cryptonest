ba7313a62f1bb188e14d5101b445cddfe2368616
Piyush Dubey <piyushdubey@Piyushs-MacBook-Air.local>
2024-08-23 10:42:27 +0530

    fixing eslint issue happening during build phase

--- components/WalletGenertor.tsx
+++ components/WalletGenertor.tsx
@@ -322,9 +322,9 @@ const OpenModal=({modalOpen,setModelOpen,handleDeleteWallet,index, setWallets,wa
                   </svg>
                   <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this Wallet?</h3>
                   <button onClick={()=> closeAndDeleteWallet()} data-modal-hide="popup-modal" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                      Yes, I'm sure
+                      Yes
                   </button>
-                  <button onClick={()=> setModelOpen(!modalOpen)} data-modal-hide="popup-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                  <button onClick={()=> setModelOpen(!modalOpen)} data-modal-hide="popup-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No</button>
               </div>
           </div>
       </div>
